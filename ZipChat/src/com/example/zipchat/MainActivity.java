9efefe5cbe341108d3063872d4a5842b2f177f23
risheeshb <risheeshb@hotmail.com>
2014-03-30 18:31:14 -0500

    ZipChat

---
+++ ZipChat/src/com/example/zipchat/MainActivity.java
@@ -0,0 +1,242 @@
+package com.example.zipchat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
+import org.jivesoftware.smack.Roster;
+import org.jivesoftware.smack.RosterEntry;
+import org.jivesoftware.smack.SASLAuthentication;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.filter.MessageTypeFilter;
+import org.jivesoftware.smack.filter.PacketFilter;
+import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smack.util.StringUtils;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+	public static final String HOST = "talk.google.com";
+	public static final int PORT = 5222;
+	public static final String SERVICE = "gmail.com";
+	public static final String USERNAME = "zipchatapp@gmail.com";
+	public static final String PASSWORD = "zipchatapp2014";
+
+	private XMPPConnection connection;
+	private ArrayList<String> messages = new ArrayList<String>();
+	private Handler mHandler = new Handler();
+
+	private EditText recipient;
+	private EditText textMessage;
+	private ListView listview;
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.fragment_main);
+
+		recipient = (EditText) this.findViewById(R.id.toET);
+		textMessage = (EditText) this.findViewById(R.id.chatET);
+		listview = (ListView) this.findViewById(R.id.listMessages);
+		setListAdapter();
+
+		// Set a listener to send a chat text message
+		Button send = (Button) this.findViewById(R.id.sendBtn);
+		send.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View view) {
+				String to = recipient.getText().toString();
+				String text = textMessage.getText().toString();
+
+				Log.i("XMPPChatDemoActivity", "Sending text " + text + " to " + to);
+				Message msg = new Message(to, Message.Type.chat);
+				msg.setBody(text);				
+				if (connection != null) {
+					connection.sendPacket(msg);
+					messages.add(connection.getUser() + ":");
+					messages.add(text);
+					setListAdapter();
+				}
+			}
+		});
+
+		connect();
+	}
+	/**
+	 * Called by Settings dialog when a connection is establised with the XMPP
+	 * server
+	 * 
+	 * @param connection
+	 */
+	public void setConnection(XMPPConnection connection) {
+		this.connection = connection;
+		if (connection != null) {
+			// Add a packet listener to get messages sent to us
+			PacketFilter filter = new MessageTypeFilter(Message.Type.chat);
+			connection.addPacketListener(new PacketListener() {
+				@Override
+				public void processPacket(Packet packet) {
+					Message message = (Message) packet;
+					if (message.getBody() != null) {
+						String fromName = StringUtils.parseBareAddress(message
+								.getFrom());
+						Log.i("XMPPChatDemoActivity", "Text Recieved " + message.getBody()
+								+ " from " + fromName );
+						messages.add(fromName + ":");
+						messages.add(message.getBody());
+						// Add the incoming message to the list view
+						mHandler.post(new Runnable() {
+							public void run() {
+								setListAdapter();
+							}
+						});
+					}
+				}
+			}, filter);
+		}
+	}
+
+	private void setListAdapter() {
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.listitem, messages);
+		listview.setAdapter(adapter);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	public void connect() {
+
+		final ProgressDialog dialog = ProgressDialog.show(this,
+				"Connecting...", "Please wait...", false);
+
+		Thread t = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				// Create a connection
+				ConnectionConfiguration connConfig = new ConnectionConfiguration(
+						HOST, PORT, SERVICE);
+				XMPPConnection connection = new XMPPConnection(connConfig);
+
+				try {
+					connection.connect();
+					Log.i("XMPPChatDemoActivity",
+							"Connected to " + connection.getHost());
+				} catch (XMPPException ex) {
+					Log.e("XMPPChatDemoActivity", "Failed to connect to "
+							+ connection.getHost());
+					Log.e("XMPPChatDemoActivity", ex.toString());
+					setConnection(null);
+				}
+				try {
+					// SASLAuthentication.supportSASLMechanism("PLAIN", 0);
+					connection.login(USERNAME, PASSWORD);
+					Log.i("XMPPChatDemoActivity",
+							"Logged in as " + connection.getUser());
+
+					// Set the status to available
+					Presence presence = new Presence(Presence.Type.available);
+					connection.sendPacket(presence);
+					setConnection(connection);
+
+					Roster roster = connection.getRoster();
+					Collection<RosterEntry> entries = roster.getEntries();
+					for (RosterEntry entry : entries) {
+						Log.d("XMPPChatDemoActivity",
+								"--------------------------------------");
+						Log.d("XMPPChatDemoActivity", "RosterEntry " + entry);
+						Log.d("XMPPChatDemoActivity",
+								"User: " + entry.getUser());
+						Log.d("XMPPChatDemoActivity",
+								"Name: " + entry.getName());
+						Log.d("XMPPChatDemoActivity",
+								"Status: " + entry.getStatus());
+						Log.d("XMPPChatDemoActivity",
+								"Type: " + entry.getType());
+						Presence entryPresence = roster.getPresence(entry
+								.getUser());
+
+						Log.d("XMPPChatDemoActivity", "Presence Status: "
+								+ entryPresence.getStatus());
+						Log.d("XMPPChatDemoActivity", "Presence Type: "
+								+ entryPresence.getType());
+						Presence.Type type = entryPresence.getType();
+						if (type == Presence.Type.available)
+							Log.d("XMPPChatDemoActivity", "Presence AVIALABLE");
+						Log.d("XMPPChatDemoActivity", "Presence : "
+								+ entryPresence);
+
+					}
+				} catch (XMPPException ex) {
+					Log.e("XMPPChatDemoActivity", "Failed to log in as "
+							+ USERNAME);
+					Log.e("XMPPChatDemoActivity", ex.toString());
+					setConnection(null);
+				}
+
+				dialog.dismiss();
+			}
+		});
+		t.start();
+		dialog.show();
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+
+}
